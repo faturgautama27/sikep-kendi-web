4e134a8496b43356bc80ba02048667a7527da399
faturgautama27 <fgstudio2708@gmail.com>
2026-06-22 10:45:34 +0700

    chore(env): setup api base url production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import type { AppEnvironment } from '@core/data-access/app-env.token';
 export const environment: AppEnvironment = {
   production: true,
   previewMode: false,
-  apiBaseUrl: '/api',
+  apiBaseUrl: 'https://apidev-sikepkendi.darmamulticorp.com/api',
   appName: 'SiKeP KenDI',
   tagline: 'Sistem Kendali Pemeliharaan Kendaraan Dinas',
   defaultLocale: 'id-ID',
