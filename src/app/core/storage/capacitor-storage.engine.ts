4b486acc4ec1d8546361cd12394c9cc60e1d1b02
faturgautama27 <fgstudio2708@gmail.com>
2026-06-24 14:36:32 +0700

    fix(apps): can't retrieve gps access

--- src/app/core/storage/capacitor-storage.engine.ts
+++ src/app/core/storage/capacitor-storage.engine.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class CapacitorStorageEngine {
-  getItem(key: string): string | null {
-    return localStorage.getItem(key);
+  getItem(key: string): Observable<string | null> {
+    return from(Preferences.get({ key })).pipe(map((res) => res.value));
   }
 
-  setItem(key: string, val: string): void {
-    localStorage.setItem(key, val);
+  setItem(key: string, val: string): Observable<void> {
+    return from(Preferences.set({ key, value: val }));
   }
 
-  removeItem(key: string): void {
-    localStorage.removeItem(key);
+  removeItem(key: string): Observable<void> {
+    return from(Preferences.remove({ key }));
   }
 }
